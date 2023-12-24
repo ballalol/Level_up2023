5ef0fe2b6b9cf1d69fa5c1055f8ef2a6c70ba037
Kira <daolian13@gmail.com>
2023-12-24 20:39:19 +0300

    добавлен поток, отображение скринсейвера

--- Snake/game.cpp
+++ Snake/game.cpp
@@ -1,7 +1,10 @@
 #include "game.h"
 #include "init.h"
 
+#include <chrono>
 #include <curses.h>
+#include <stdlib.h>
+#include <time.h>
 
 void InitGame(GAME* game)
 {
@@ -16,12 +19,20 @@ void InitGame(GAME* game)
     keypad(stdscr, true);
 
     getmaxyx(stdscr, game->height, game->width);
+    game->current_state = CURRENT_STATE::IN_SCREEN;
+    game->th = new std::thread (GameStarter, game);
 }
 void DeinitGame(GAME *game)
 {
     if (game == nullptr) {
         return;
     }
+    if (game->th && game->th->joinable())
+    {
+        game->th->join();
+    }
+
+    delete game->th;
     endwin();
 }
 void StartGame(GAME *game)
@@ -29,6 +40,50 @@ void StartGame(GAME *game)
     if (game == nullptr) {
         return;
     }
-    printw("Start game");
+    int cur;
+    do {
+        cur = getch();
+        switch (game->current_state) {
+        case CURRENT_STATE::IN_SCREEN:
+            ScreenKeyer(game, cur);
+            break;
+        }
+    }
+    while (game->current_state != IN_EXIT);
+}
+void ScreenKeyer(GAME *game, int cur){
     getch();
+    game->current_state = CURRENT_STATE::IN_EXIT;
+}
+void ScreenShower (GAME *game)
+{
+    printw("SNAKE");
+}
+void GameStarter (GAME *game)
+{
+    if (game == nullptr) {
+        return;
+    }
+    auto begin = std::chrono::steady_clock::now();
+    while (game->current_state != CURRENT_STATE::IN_EXIT) {
+        const auto now = std::chrono::steady_clock::now();
+        const auto diff = now - begin;
+        bool changes = false;
+        switch (game->current_state) {
+        case CURRENT_STATE::IN_SCREEN:
+            changes = ScreenAction(game, std::chrono::duration_cast < std::chrono::milliseconds > (diff));
+
+        }
+
+    }
+}
+bool ScreenAction (GAME *game, std::chrono::milliseconds ms)
+{
+    if(ms.count() > 30){
+        clear();
+        ScreenShower(game);
+        refresh();
+        return true;
+    }
+    return false;
 }
--- Snake/game.h
+++ Snake/game.h
@@ -1,13 +1,29 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <chrono>
+#include <thread>
+
+enum CURRENT_STATE{
+    IN_SCREEN,
+    IN_MENU,
+    IN_GAME,
+    IN_EXIT
+};
+
 struct GAME
 {
     int width;
     int height;
+    int current_state;
+    std::thread* th;
 };
 void InitGame(GAME* game);
 
 void DeinitGame(GAME *game);
 void StartGame(GAME *game);
+void ScreenKeyer(GAME *game, int cur);
+void ScreenShower(GAME *game);
+void GameStarter (GAME *game);
+bool ScreenAction (GAME *game, std::chrono::milliseconds ms);
 #endif // GAME_H
